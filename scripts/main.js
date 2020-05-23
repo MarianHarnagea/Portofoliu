e01a8fd032f963d7876456c1bc532565abc34143
MarianHarnagea <marianharnagea.com>
2020-05-23 12:02:41 +0300

    master

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+//Night Mode
+const switchBtn = document.querySelector(".switch-container");
+const body = document.querySelector("body");
+const d = new Date();
+const hours = d.getHours();
+const night = hours >= 19 || hours <= 7; // between 7pm and 7am
+
+if (night) {
+  body.classList.add("night");
+  switchBtn.classList.add("active");
+}
+
+switchBtn.addEventListener("click", () => {
+  if (switchBtn.classList.toggle("active")) {
+    body.classList.add("night");
+  } else {
+    body.classList.remove("night");
+  }
+});
+//Night Mode End
+
+//Scroll Animations
+AOS.init({
+  duration: 1000,
+  easing: "ease"
+});
+//Scroll Animations End
+
+//Scroll To Top
+const topBtn = document.querySelector(".top");
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+});
+//Scroll To Top End
+
+// Patricles
+particlesJS.load("particles-js", "./scripts/particles.json", function() {});
